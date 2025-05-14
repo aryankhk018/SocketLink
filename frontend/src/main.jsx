0d84068cece7562f6000922634af40aa3fa329cf
aryankhk018 <160753150+aryankhk018@users.noreply.github.com>
2025-05-14 17:53:39 +0530

    second attempt to deploy frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import Landing from "./pages/landing.jsx";
-import Authenctication from "./pages/Authentication.jsx";
-import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
