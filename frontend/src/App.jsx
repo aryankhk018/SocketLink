3a8b1b16cbd78a1089331b00e3084d65fea9d927
aryankhk018 <160753150+aryankhk018@users.noreply.github.com>
2025-08-29 06:30:09 +0530

    refresh issue tryle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,47 @@
-import { useState } from "react";
-
 import "./App.css";
 import Landing from "./pages/Landing";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Authenctication from "./pages/Authentication";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import VideoMeet from "./pages/VideoMeet.jsx";
 import Home from "./pages/Home.jsx";
 import History from "./pages/History.jsx";
-import VideoMeetComponent from "./pages/VideoMeet.jsx";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Landing />,
-    },
-    {
-      path: "/auth",
-      element: <Authenctication />,
-    },
-    {
-      path: "/:url",
-      element: <VideoMeet />,
-    },
-    {
-      path: "/home",
-      element: <Home />,
-    },
-    {
-      path: "/history",
-      element: <History />,
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Landing />,
+  //   },
+  //   {
+  //     path: "/auth",
+  //     element: <Authenctication />,
+  //   },
+  //   {
+  //     path: "/:url",
+  //     element: <VideoMeet />,
+  //   },
+  //   {
+  //     path: "/home",
+  //     element: <Home />,
+  //   },
+  //   {
+  //     path: "/history",
+  //     element: <History />,
+  //   },
+  // ]);
   return (
     <>
       <AuthProvider>
-        <RouterProvider router={router}></RouterProvider>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/auth" element={<Authenctication />} />
+            <Route path="/:url" element={<VideoMeet />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </HashRouter>
       </AuthProvider>
     </>
   );
